c8bc07477f86f125fb2561eb230bcb2f5aaeb382
Bryan Garduno Gonzalez <bryangonzalez1896@icloud.com>
2023-08-04 11:03:25 -0500

    imports added

--- src/pages/Global/NavBar.jsx
+++ src/pages/Global/NavBar.jsx
@@ -1,4 +1,13 @@
-import React from 'react';
+import { useDispatch, useSelector } from "react-redux";
+import { Badge, Box, IconButton } from '@mui/material';
+import {
+  PersonOutline,
+  ShoppingBagOutlined,
+  MenuOutlined,
+  SearchOutlined
+} from '@mui/icons-material';
+import { useNavigate } from "react-router-dom";
+import { shades } from '../../theme';
 
 const NavBar = () => {
   return (
