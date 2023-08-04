3f9245fabb46f6653df0beaea9228d16133a771a
Bryan Garduno Gonzalez <bryangonzalez1896@icloud.com>
2023-08-04 10:19:45 -0500

    ThemeProvider rendered

--- src/index.js
+++ src/index.js
@@ -10,6 +10,9 @@ import { theme } from './theme';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
