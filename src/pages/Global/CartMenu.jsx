e8e97169ed4b12467f7c5f08b395d1f71bf1d15a
Bryan Garduno Gonzalez <bryangonzalez1896@icloud.com>
2023-08-04 11:54:34 -0500

    Styles and onClick added

--- src/pages/Global/CartMenu.jsx
+++ src/pages/Global/CartMenu.jsx
@@ -6,10 +6,10 @@ import RemoveIcon from "@mui/icons-material/Remove";
 import styled from "@emotion/styled";
 import { shades } from "../../theme";
 import {
-  decreaseCount,
-  increaseCount,
-  removeFromCart,
-  setIsCartOpen,
+    decreaseCount,
+    increaseCount,
+    removeFromCart,
+    setIsCartOpen,
 } from "../../state";
 import { useNavigate } from "react-router-dom";
 
@@ -30,30 +30,37 @@ const CartMenu = () => {
     }, 0);
 
 
-  return (
-    <Box
-        display={isCartOpen ? "block" : "none"}
-        backgroundColor="rgba(0, 0, 0, 0.4)"
-        position="fixed"
-        zIndex={10}
-        width="100%"
-        height="100%"
-        left="0"
-        top="0"
-        overflow="auto"
-    >
-        <Box>
-            <Box>
-                <FlexBox>
-                    <Typography>Shopping Bag</Typography>
-                    <IconButton>
-                        <CloseIcon />
-                    </IconButton>
-                </FlexBox>
+    return (
+        <Box
+            display={isCartOpen ? "block" : "none"}
+            backgroundColor="rgba(0, 0, 0, 0.4)"
+            position="fixed"
+            zIndex={10}
+            width="100%"
+            height="100%"
+            left="0"
+            top="0"
+            overflow="auto"
+        >
+            <Box
+                position="fixed"
+                right="0"
+                bottom="0"
+                width="max(400px, 30%)"
+                height="100%"
+                backgroundColor="white"
+            >
+                <Box padding="30px" overflow="auto" height="100%">
+                    <FlexBox mb="15px">
+                        <Typography variant="h3">SHOPPING BAG ({cart.length})</Typography>
+                        <IconButton onClick={() => dispatch(setIsCartOpen({}))}>
+                            <CloseIcon />
+                        </IconButton>
+                    </FlexBox>
+                </Box>
             </Box>
         </Box>
-    </Box>
-  )
+    )
 }
 
 export default CartMenu;
