c8e8530a04a990595eb1a6085d6c9f3e56d7ee5c
Bryan Garduno Gonzalez <bryangonzalez1896@icloud.com>
2023-08-04 10:11:34 -0500

    primary color shades added

---
+++ src/theme.js
@@ -0,0 +1,20 @@
+import { createTheme } from "@mui/material/styles";
+
+export const shades = {
+    primary: {
+        100: "#cccccc",
+        200: "#999999",
+        300: "#666666",
+        400: "#333333",
+        500: "#000000",
+        600: "#000000",
+        700: "#000000",
+        800: "#000000",
+        900: "#000000"
+    },
+}
+
+
+#d6001c
+
+#cfcdcd
