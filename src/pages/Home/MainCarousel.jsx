06e29f0f4e44c658d79244b092e232cb1f3a9d8c
Bryan Garduno Gonzalez <bryangonzalez1896@icloud.com>
2023-08-05 10:04:33 -0500

    Importall setup to import all images from assets folder

--- src/pages/Home/MainCarousel.jsx
+++ src/pages/Home/MainCarousel.jsx
@@ -5,6 +5,18 @@ import NavigateBeforeIcon from "@mui/icons-material/NavigateBefore";
 import NavigateNextIcon from "@mui/icons-material/NavigateNext";
 import { shades } from "../../theme";
 
+// import all images from assets folder. 
+const importAll = (r) => 
+  r.keys().reduce((acc, item) => {
+    acc[item.replace('./', '')] = r(item);
+    return acc;
+  }, {});
+
+  export const heroTextureImports = importAll(
+    require.context("../../assets", false, /\.(png|jpe?g|svg)$/)
+  );
+
+
 const MainCarousel = () => {
   return (
     <div>
