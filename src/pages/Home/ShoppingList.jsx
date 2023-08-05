0d2d86dd73f4a45347ee35ca053c675563f51bc5
Bryan Garduno Gonzalez <bryangonzalez1896@icloud.com>
2023-08-05 15:07:36 -0500

    styles added

--- src/pages/Home/ShoppingList.jsx
+++ src/pages/Home/ShoppingList.jsx
@@ -44,8 +44,8 @@ const ShoppingList = () => {
 
 
   return (
-    <Box>
-      <Typography>Our Featured <b>Products</b></Typography>
+    <Box width="80%" margin="80px auto">
+      <Typography variant="h3" textAlign="center">Our Featured <b>Products</b></Typography>
     </Box>
   )
 }
