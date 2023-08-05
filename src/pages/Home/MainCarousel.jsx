8afb9060aa45a56ad1e7f882e47d5e770aa2bfcf
Bryan Garduno Gonzalez <bryangonzalez1896@icloud.com>
2023-08-05 10:11:14 -0500

    Before and Next icon sections added

--- src/pages/Home/MainCarousel.jsx
+++ src/pages/Home/MainCarousel.jsx
@@ -6,25 +6,60 @@ import NavigateNextIcon from "@mui/icons-material/NavigateNext";
 import { shades } from "../../theme";
 
 // import all images from assets folder. 
-const importAll = (r) => 
+const importAll = (r) =>
   r.keys().reduce((acc, item) => {
     acc[item.replace('./', '')] = r(item);
     return acc;
   }, {});
 
-  const heroTextureImports = importAll(
-    require.context("../../assets", false, /\.(png|jpe?g|svg)$/)
-  );
+const heroTextureImports = importAll(
+  require.context("../../assets", false, /\.(png|jpe?g|svg)$/)
+);
 
 
 const MainCarousel = () => {
   const isNonMobile = useMediaQuery('(min-width:600px)');
 
-  
+
   return (
-    <div>
-      <h1>Main Carousel</h1>
-    </div>
+    <Carousel
+      infiniteLoop={true}
+      showThumbs={false}
+      showIndicators={false}
+      showStatus={false}
+      renderArrowPrev={(onClickHandler, hasPrev, label) => (
+        <IconButton
+          onClick={onClickHandler}
+          sx={{
+            position: "absolute",
+            top: '50%',
+            left: '0',
+            color: 'white',
+            padding: '5px',
+            zIndex: '10',
+          }}
+        >
+          <NavigateBeforeIcon sx={{ fontSize: 40 }} />
+        </IconButton>
+      )}
+      renderArrowNext={(onClickHandler, hasNext, label) => (
+        <IconButton
+          onClick={onClickHandler}
+          sx={{
+            position: "absolute",
+            top: "50%",
+            right: "0",
+            color: "white",
+            padding: "5px",
+            zIndex: "10",
+          }}
+        >
+          <NavigateNextIcon sx={{ fontSize: 40 }} />
+        </IconButton>
+      )}
+    >
+
+    </Carousel>
   );
 }
 
