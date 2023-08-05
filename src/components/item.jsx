7f7828e8c91faee07dff40474736518ee8e0061c
Bryan Garduno Gonzalez <bryangonzalez1896@icloud.com>
2023-08-05 09:39:02 -0500

    Remove, Add Icon's added, Add to cart buttona dded

--- src/components/item.jsx
+++ src/components/item.jsx
@@ -7,7 +7,7 @@ import { shades } from "../theme";
 import { addToCart } from "../state";
 import { useNavigate } from "react-router-dom";
 
-const item = ({ item, width}) => {
+const item = ({ item, width }) => {
     const navigate = useNavigate();
     const dispatch = useDispatch();
     const [count, setCount] = useState(1);
@@ -16,7 +16,7 @@ const item = ({ item, width}) => {
         palette: { neutral },
     } = useTheme();
     const { category, price, name, image } = item.attributes;
-    const { 
+    const {
         data: {
             attributes: {
                 formats: {
@@ -26,23 +26,57 @@ const item = ({ item, width}) => {
         }
     } = image;
 
-  return (
-    <Box width={width}>
-        <Box
-            position="relative"
-            onMouseOver={() => setIsHovered(true)}
-            onMouseOut={() => setIsHovered(false)}
-        >
-            <img 
-                alt={item.name}
-                width="300px"
-                height="400px"
-                src={`http://localhost:1337${url}`}
-                onClick={() => navigate(`/item/${item.id}`)}
-            />
-        </Box>
-    </Box>
-  );
+    return (
+        <Box width={width}>
+            <Box
+                position="relative"
+                onMouseOver={() => setIsHovered(true)}
+                onMouseOut={() => setIsHovered(false)}
+            >
+                <img
+                    alt={item.name}
+                    width="300px"
+                    height="400px"
+                    src={`http://localhost:1337${url}`}
+                    onClick={() => navigate(`/item/${item.id}`)}
+                    style={{ cursor: 'pointer' }}
+                />
+                <Box
+                    display={isHovered ? "blocked" : "none"}
+                    position="absolute"
+                    bottom="10%"
+                    left="0"
+                    width="100%"
+                    padding=" 0 5%"
+                >
+                    <Box display="flex" justifyContent="space-between">
+                        <Box
+                            display="flex"
+                            alignItems="center"
+                            backgroundColor={shades.neutral[100]}
+                            borderRadius="3px"
+                        >
+                            <IconButton onClick={() => setCount(Math.max(count - 1, 1))}>
+                                <RemoveIcon />
+                            </IconButton>
+                            <Typography color={shades.primary[300]}>{count}</Typography>
+                            <IconButton onClick={() => setCount(count + 1)}>
+                                <AddIcon />
+                            </IconButton>
+                        </Box>
+                        <Button
+                            onClick={() => {
+                                dispatch(addToCart({ item: { ...item, count } }));
+                            }}
+                            sx={{ backgroundColor: shades.primary[300], color: "white" }}
+                        >
+                            Add to Cart
+                        </Button>
+                    </Box>
+                </Box>
+            </Box>
+        </Box >
+    );
 };
 
 export default item;
