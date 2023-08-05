3e79f7ff8e38eea5eeb564b144656c5f7eed283a
Bryan Garduno Gonzalez <bryangonzalez1896@icloud.com>
2023-08-05 14:29:21 -0500

    Typography Box styles added

--- src/pages/Home/MainCarousel.jsx
+++ src/pages/Home/MainCarousel.jsx
@@ -71,7 +71,19 @@ const MainCarousel = () => {
               backgroundAttachment: 'fixed',
             }}
           />
-          <Box>
+          <Box
+            padding="20px"
+            borderRadius="1px"
+            textAlign="left"
+            backgroundColor="rgb(0, 0, 0, 0.4)"
+            position="absolute"
+            top="46%"
+            left={isNonMobile ? "10%" : "0"}
+            right={isNonMobile ? undefined : "0"}
+            margin={isNonMobile ? undefined : "0 auto"}
+            maxWidth={isNonMobile ? undefined : "240px"}
+            color="white"
+          >
             <Typography>-- NNEW ITEMS</Typography>
             <Typography>Summer Sale</Typography>
             <Typography>
