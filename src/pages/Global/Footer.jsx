885b5655ec9e3c54c294bb580891cba87a8a971b
Bryan Garduno Gonzalez <bryangonzalez1896@icloud.com>
2023-08-05 15:42:06 -0500

    parent container styles added

--- src/pages/Global/Footer.jsx
+++ src/pages/Global/Footer.jsx
@@ -10,8 +10,16 @@ function Footer() {
 
 
     return (
-        <Box>
-            <Box>
+        <Box marginTop="70px" padding="40px 0" backgroundColor={neutral.light}>
+            <Box
+                width="80%"
+                margin="auto"
+                display="flex"
+                justifyContent="space-between"
+                flexWrap="wrap"
+                rowGap="30px"
+                columnGap="clamp(20px, 30px, 40px)"
+            >
                 <Box>
                     <Typography>
                         Ecommerce Website
