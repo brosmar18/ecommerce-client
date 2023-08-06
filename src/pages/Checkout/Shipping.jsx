2f77e685fb8dace1a836ec6115ef4db867e5d9c7
Bryan Garduno Gonzalez <bryangonzalez1896@icloud.com>
2023-08-06 14:03:33 -0500

    file added

--- src/pages/Checkout/Shipping.jsx
+++ src/pages/Checkout/Shipping.jsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import { Box, Checkout, FormControlLabel, Typography } from '@mui/material';
+import AddressForm from './AddressForm';
 
 const Shipping = () => {
   return (
