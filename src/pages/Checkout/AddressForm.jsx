9f112fa930cca10973ef4d4e06180cb47d4373e1
Bryan Garduno Gonzalez <bryangonzalez1896@icloud.com>
2023-08-06 14:22:40 -0500

    format functions added

--- src/pages/Checkout/AddressForm.jsx
+++ src/pages/Checkout/AddressForm.jsx
@@ -12,6 +12,16 @@ const AddressForm = ({
     handleChange,
 }) => {
     const isNonMobile = useMediaQuery('(min-width:600px)');
+
+    // improve readability
+    const formattedName = (field) => `${type}.${field}`;
+
+    const formattedError = (field) => Boolean(
+        getIn(touched, formattedName(field) && getIn(errors, formattedName(field)))
+    );
+
+    const formattedHelper = (field) =>
+        getIn(touched, formattedName(field)) && getIn(errors, formattedName(field));
     return (
         <div>
             Address Form
