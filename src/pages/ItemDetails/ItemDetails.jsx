91f6c5b30c65c7a57be678f67fa2ecc8b2694b28
Bryan Garduno Gonzalez <bryangonzalez1896@icloud.com>
2023-08-06 13:37:20 -0500

    styles added

--- src/pages/ItemDetails/ItemDetails.jsx
+++ src/pages/ItemDetails/ItemDetails.jsx
@@ -123,9 +123,15 @@ const ItemDetails = () => {
         )}
         {value === 'reviews' && <div>reviews</div>}
       </Box>
-      <Box>
-        <Typography>Related Products</Typography>
-        <Box>
+      <Box mt="50px" width="100%">
+        <Typography variant="h3" fontWeight="bold">Related Products</Typography>
+        <Box
+          mt="20px"
+          display="flex"
+          flexWrap="wrap"
+          columnGap="1.33%"
+          justifyContent="space-between"
+        >
           {items.slice(0, 4).map((item, i) => (
             <Item key={`${item.name}-${i}`} item={item} />
           ))}
