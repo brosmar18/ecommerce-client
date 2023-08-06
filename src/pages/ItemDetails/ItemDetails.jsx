f8dafe0793ae3c4aba5b02d02c029f0e89ac4651
Bryan Garduno Gonzalez <bryangonzalez1896@icloud.com>
2023-08-06 13:06:31 -0500

    imports added

--- src/pages/ItemDetails/ItemDetails.jsx
+++ src/pages/ItemDetails/ItemDetails.jsx
@@ -1,4 +1,16 @@
-import React from 'react';
+import { Box, Button, IconButton, Typography } from "@mui/material";
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import Item from "../../components/Item";
+import FavoriteBorderOutlinedIcon from "@mui/icons-material/FavoriteBorderOutlined";
+import AddIcon from "@mui/icons-material/Add";
+import RemoveIcon from "@mui/icons-material/Remove";
+import { shades } from "../../theme";
+import { addToCart } from "../../state";
+import { useDispatch } from "react-redux";
+
 
 const ItemDetails = () => {
   return (
