63b54fcb16bb48ebe28244e661a96471f99a8c9a
Bryan Garduno Gonzalez <bryangonzalez1896@icloud.com>
2023-08-06 14:20:06 -0500

    props added

--- src/pages/Checkout/AddressForm.jsx
+++ src/pages/Checkout/AddressForm.jsx
@@ -3,12 +3,19 @@ import { Box } from "@mui/material";
 import TextField from "@mui/material/TextField";
 import useMediaQuery from "@mui/material/useMediaQuery";
 
-const AddressForm = () => {
-  return (
-    <div>
-      Address Form
-    </div>
-  )
+const AddressForm = ({
+    type,
+    values,
+    touched,
+    errors,
+    handleBlur,
+    handleChange,
+}) => {
+    return (
+        <div>
+            Address Form
+        </div>
+    )
 }
 
 export default AddressForm;
