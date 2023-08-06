d66ad114d759dd95609943521bae922965794645
Bryan Garduno Gonzalez <bryangonzalez1896@icloud.com>
2023-08-06 14:44:53 -0500

    email text field added

--- src/pages/Checkout/Payment.jsx
+++ src/pages/Checkout/Payment.jsx
@@ -1,11 +1,26 @@
-import React from 'react';
+import { Box, Typography } from '@mui/material';
+import TextField from '@mui/material/TextField';
 
-const Payment = () => {
-  return (
-    <div>
-      <h1>Payment</h1>
-    </div>
-  )
+const Payment = ({ values, touched, errors, handleBlur, handleChange }) => {
+    return (
+        <Box>
+            <Typography sx={{ mg: '15px' }} fontSize='18px'>
+                Contact Info
+            </Typography>
+            <TextField
+                fullWidth
+                type="text"
+                label="Email"
+                onBlur={handleBlur}
+                onChange={handleChange}
+                value={values.email}
+                name="email"
+                error={!!touched.email && !!errors.email}
+                helperText={touched.email && errors.email}
+                sx={{ gridColumn: "span 4", marginBottom: "15px" }}
+            />
+        </Box>
+    )
 }
 
 export default Payment;
